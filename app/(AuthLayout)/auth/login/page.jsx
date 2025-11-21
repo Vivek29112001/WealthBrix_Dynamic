777c0d73d93fe1dc980451c2ca2381924d5faa10
vivek sharma <vivek15292001@gmail.com>
2025-11-21 12:36:08 +0530

    Setup CI/CD workflow

--- app/(AuthLayout)/auth/login/page.jsx
+++ app/(AuthLayout)/auth/login/page.jsx
@@ -1,12 +1,14 @@
 import LoginComponent from '@/components/authentication/auth/login'
+import Seo from '@/shared/seo/seo'
 import React from 'react'
 
 const LoginPage = () => {
     return (
         <>
+            <Seo title="Login" />
             <div className="min-h-screen w-full flex items-center justify-center bg-gray-50">
                 <LoginComponent />
-            </div>
+            </div >
         </>
     )
 }
